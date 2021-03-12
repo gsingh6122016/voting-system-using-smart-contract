717365dc6ebb45be9c1b7815201077281be65798
= <=>
2021-03-12 15:11:01 +0530

    frontend setup done

--- backend.py
+++ backend.py
@@ -12,7 +12,7 @@ web3 = Web3(Web3.HTTPProvider(rpc))
 
 abi = '[{"constant":false,"inputs":[{"name":"_candidateId","type":"uint256"}],"name":"vote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"candidatesCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidates","outputs":[{"name":"id","type":"uint256"},{"name":"name","type":"string"},{"name":"voteCount","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"voters","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"end","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_candidateId","type":"uint256"}],"name":"votedEvent","type":"event"}]'
 
-contract_addr = "0x793549111784530fDD40DBD49F1f3286D70631dF"
+contract_addr = "0x3Bf7E6310939aE8505643ef22c14dcD763FCe3Cf"
 
 app = Flask(__name__)
 app.secret_key = 'i love white chocolate too'
